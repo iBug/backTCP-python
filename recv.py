e57fc7cb94bdc78de89b46af0243d980e9c9518e
iBug <git@ibugone.com>
2019-11-07 14:40:25 +0800

    Implement entry points send.py and recv.py

---
+++ recv.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import argparse
+
+import backTCP
+from utils import *
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="receive a file from backTCP", epilog="This program is created by iBug")
+    parser.add_argument('filename', metavar="file", help="the name to save received file as")
+    parser.add_argument('-a', '-A', '--address', metavar="addr", help="address to listen for", default="0.0.0.0")
+    parser.add_argument('-p', '--port', metavar="port", help="port to listen on", default=6666)
+    parser.add_argument('-l', '--log-level', metavar="level", help="logging level", default=LOG_WARNING)
+    return parser.parse_args()
+
+
+def main():
+    global log_level
+    args = parse_args()
+    log_level = args.log_level
+
+    with open(args.filename, "wb") as f:
+        f.write(backTCP.recv(args.address, args.port))
+
+
+if __name__ == '__main__':
+    main()
---
+++ send.py
@@ -0,0 +1,29 @@
+import sys
+import os
+import argparse
+
+import backTCP
+from utils import *
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="send a file via backTCP", epilog="This program is created by iBug")
+    parser.add_argument('filename', metavar="file", help="the name of the file to send")
+    parser.add_argument('-a', '-A', '--address', metavar="addr", help="address of target to send", default="127.0.0.1")
+    parser.add_argument('-p', '--port', metavar="port", help="port of target to send", default=6666)
+    parser.add_argument('-l', '--log-level', metavar="level", help="logging level", default=LOG_WARNING)
+    return parser.parse_args()
+
+
+def main():
+    global log_level
+    args = parse_args()
+    log_level = args.log_level
+
+    with open(args.filename, "rb") as f:
+        data = f.read()
+    backTCP.send(data, args.address, args.port)
+
+
+if __name__ == '__main__':
+    main()
