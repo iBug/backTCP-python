fbaeaa1acbc4c60c6956486e21751ced9bb7d62b
iBug <git@ibugone.com>
2019-11-16 21:46:19 +0800

    Allow override of recv(2) size

--- backTCP.py
+++ backTCP.py
@@ -50,8 +50,8 @@ class BTcpConnection:
             packet = b''
         self.conn.sendall(bytes(packet))
 
-    def recv(self):
-        return BTcpPacket.from_bytes(self.conn.recv(7 + 64))
+    def recv(self, size=None):
+        return BTcpPacket.from_bytes(self.conn.recv(size or (7 + 64)))
 
 
 
